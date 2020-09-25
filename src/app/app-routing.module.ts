4f4daf9b813accf5140a0f0dec63b3644b42e8b8
caubebuon03 <70497356+caubebuon03@users.noreply.github.com>
2020-09-25 09:35:21 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ChitietComponent } from './chitiet/chitiet.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: 'chitiet',
+    component: ChitietComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppRoutingModule { }
